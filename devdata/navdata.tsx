593c23ffaa103382575de478387424786a1e59be
aLearningLad <mocmanca@gmail.com>
2024-06-12 23:53:44 +0200

    navdata additions

--- devdata/navdata.tsx
+++ devdata/navdata.tsx
@@ -1,1 +1,53 @@
-export const navdata = [];
+import { Tnavdata } from "@/types";
+
+export const navdata: Tnavdata[] = [
+  {
+    linkId: 1,
+    linkname: "Alerts",
+    linkref: "/alerts",
+    icon: <p></p>,
+  },
+  {
+    linkId: 2,
+    linkname: "Home",
+    linkref: "/home",
+    icon: <p></p>,
+  },
+  {
+    linkId: 3,
+    linkname: "Disbursed Loans",
+    linkref: "/disbursed",
+    icon: <p></p>,
+  },
+  {
+    linkId: 4,
+    linkname: "Bookmarks",
+    linkref: "/bookmarks",
+    icon: <p></p>,
+  },
+  {
+    linkId: 5,
+    linkname: "Peers",
+    linkref: "/peers",
+    icon: <p></p>,
+  },
+  // REMEMBER, IGNORE ICON ABOVE SETTINGS. NOT INCLUDING IT HERE
+  {
+    linkId: 6,
+    linkname: "Settings",
+    linkref: "/alerts",
+    icon: <p></p>,
+  },
+  {
+    linkId: 7,
+    linkname: "Create Listing",
+    linkref: "/create",
+    icon: <p></p>,
+  },
+  //   {
+  //     linkId: 1,
+  //     linkname: "Alerts",
+  //     linkref: "/alerts",
+  //     icon: <p></p>,
+  //     },
+];
