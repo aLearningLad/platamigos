6a159f5fbeb433f1f0d5fa4bc052b4a5d1e24f8a
aLearningLad <mocmanca@gmail.com>
2024-06-15 23:03:55 +0200

    btn changes

--- components/foruserhome/ChatBtn.tsx
+++ components/foruserhome/ChatBtn.tsx
@@ -1,7 +1,23 @@
 "use client";
 
+import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
+
 const ChatBtn = () => {
-  return <button>ChatBtn</button>;
+  const handleChatBtn = () => {
+    alert("Open chat modal here!");
+  };
+
+  return (
+    <button
+      onClick={handleChatBtn}
+      className=" w-fit h-fit bg-neutral-900 flex justify-center items-center py-4 md:py-5  xl:py-1 px-2 rounded-lg text-xl lg:text-[12px] text-white"
+    >
+      <IoChatbubbleEllipsesSharp />{" "}
+      <p className=" text-[10px] text-white">
+        Negotiate with <i>Peers</i>{" "}
+      </p>
+    </button>
+  );
 };
 
 export default ChatBtn;
