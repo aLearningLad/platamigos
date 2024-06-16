2eb096819425eed4fda9496d87fb19ac9bce5595
aLearningLad <mocmanca@gmail.com>
2024-06-16 23:10:35 +0200

    ui additions to feed loan card box

--- components/foruserhome/PriorityRight.tsx
+++ components/foruserhome/PriorityRight.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import FeedLoanMiniCard from "./FeedLoanMiniCard";
+import { FaAngleDoubleDown } from "react-icons/fa";
 
 const PriorityRight = () => {
   return (
-    <div className=" h-full w-full lg:w-[30%] overflow-auto border-4 border-white flex flex-col items-center gap-3 md:gap-5 lg:gap-8">
+    <div className="h-full relative w-full lg:w-[30%] overflow-auto px-1 md:px-2 lg:px-3 flex flex-col items-center gap-3 md:gap-5">
       <FeedLoanMiniCard />
+      <FeedLoanMiniCard />
+      <FeedLoanMiniCard />
+      <FeedLoanMiniCard />
+      <FeedLoanMiniCard />
+      <FeedLoanMiniCard />
+      <FeedLoanMiniCard />
+      <span className=" sticky bottom-0 w-full flex justify-center items-center flex-col">
+        <p className=" text-lg text-neutral-50">Scroll down</p>
+        <FaAngleDoubleDown size={18} className=" text-neutral-50" />
+      </span>
     </div>
   );
 };
