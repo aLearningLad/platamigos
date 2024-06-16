e313dcd53f2fb799a9bfcb0684608ed87ae30785
aLearningLad <mocmanca@gmail.com>
2024-06-16 22:58:11 +0200

    mini loan card additional changes

--- components/foruserhome/FeedLoanMiniCard.tsx
+++ components/foruserhome/FeedLoanMiniCard.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const FeedLoanMiniCard = () => {
-  return <div>FeedLoanMiniCard</div>;
+  return (
+    <div className=" text-white min-h-[25vh] md:min-h-[30vh] lg:min-h-[20vh] w-full p-1 md:p-2 lg:p-3 border-2 border-white rounded-md lg:rounded-lg ">
+      FeedLoanMiniCard
+    </div>
+  );
 };
 
 export default FeedLoanMiniCard;
--- components/foruserhome/PriorityRight.tsx
+++ components/foruserhome/PriorityRight.tsx
@@ -3,7 +3,7 @@ import FeedLoanMiniCard from "./FeedLoanMiniCard";
 
 const PriorityRight = () => {
   return (
-    <div className=" h-full w-full lg:w-[30%] border-4 border-white flex flex-col items-center gap-3 md:gap-5 lg:gap-8">
+    <div className=" h-full w-full lg:w-[30%] overflow-auto border-4 border-white flex flex-col items-center gap-3 md:gap-5 lg:gap-8">
       <FeedLoanMiniCard />
     </div>
   );
