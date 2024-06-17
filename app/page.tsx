f3fbfc8ffbe83c8a88b0aa44f441adc1d611b11c
aLearningLad <mocmanca@gmail.com>
2024-06-17 22:17:44 +0200

    added more auth control

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import { GoogleAuthBtn } from "@/components/utilComps";
+import { createClient } from "@/utils/supabase/server";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+
+  const userIs = await (await supabase.auth.getUser()).data.user?.user_metadata;
+  console.log("Here's the user from home page: ", userIs);
   return (
     <main className="flex w-full h-full flex-col bg-black items-center justify-center ">
       <p className=" text-3xl text-white">
