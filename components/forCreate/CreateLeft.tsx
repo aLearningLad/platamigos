311867eecb24b61d168fe4eb9694eabd656f27c8
aLearningLad <mocmanca@gmail.com>
2024-06-18 16:41:39 +0200

    further progress, css additions

--- components/forCreate/CreateLeft.tsx
+++ components/forCreate/CreateLeft.tsx
@@ -1,8 +1,33 @@
 "use client";
+
+import { useState } from "react";
+
 const CreateLeft = () => {
+  const [title, setTitle] = useState<string>("");
+  const [purpose, setPurpose] = useState<string>("");
+  const [expiryDate, setExpiryDate] = useState<Date | null>(null);
+  const [interestRate, setInterestRate] = useState<number | null>(null);
+  const [principle, setPrinciple] = useState<number | null>(null);
+  const [instalment, setInstalment] = useState<number | null>(null);
+  const [totalDue, setTotalDue] = useState<number | null>(null);
+
   return (
-    <section className="w-full h-[70vh] md:h-[65vh] lg:h-full lg:w-1/2 flex flex-col items-center justify-around p-1 md:p-2 lg:p-4 xl:p-7 border-2 border-white">
-      CreateLeft
+    <section className="w-full h-[70vh] md:h-[65vh] lg:h-full lg:w-1/2 flex flex-col items-center justify-around p-1 md:p-2 lg:p-4 xl:p-7 border-2 border-white bg-gradient-to-br from-cyan-800/50 via-black to-black">
+      {/* TITLE */}
+      <div className=" w-full h-full flex flex-col items-start ">
+        <label htmlFor="title">Loan Title</label>
+        <input type="text" className="inputField" />
+      </div>
+
+      {/* PURPOSE */}
+
+      {/* INTEREST RATE */}
+
+      {/* PRINCIPLE AMOUNT */}
+
+      {/* INSTALMENT AMOUNT */}
+
+      {/* TOTAL DUE */}
     </section>
   );
 };
--- components/forCreate/CreateLoanComp.tsx
+++ components/forCreate/CreateLoanComp.tsx
@@ -5,9 +5,16 @@ import CreateRight from "./CreateRight";
 
 const CreateLoanComp = () => {
   return (
-    <div className="flex-col lg:flex-row flex justify-center items-center w-full h-full bg-red-600">
-      <CreateLeft />
-      <CreateRight />
+    <div className="flex-col  flex justify-center items-center w-full h-full">
+      <header className=" w-full flex h-fit py-3 lg:py-4 justify-center items-center">
+        <h1 className=" text-4xl md:text-3xl lg:text-2xl text-white">
+          Loan Offer Details
+        </h1>
+      </header>
+      <div className=" flex flex-col lg:flex-row">
+        <CreateLeft />
+        <CreateRight />
+      </div>
     </div>
   );
 };
