95387a8508f07e291d7556efa0a6da4858095f14
aLearningLad <mocmanca@gmail.com>
2024-06-18 18:59:27 +0200

    create loan ui additions

--- components/forCreate/CreateLeft.tsx
+++ components/forCreate/CreateLeft.tsx
@@ -12,9 +12,9 @@ const CreateLeft = () => {
   const [totalDue, setTotalDue] = useState<number | null>(null);
 
   return (
-    <section className="w-full h-[70vh] md:h-[65vh] lg:h-full lg:w-1/2 flex flex-col items-start justify-around p-1 md:p-2 lg:p-4 xl:p-7 border-2 border-white bg-gradient-to-br from-cyan-800/50 via-black to-black">
+    <section className="w-full h-[70vh] md:h-[65vh] lg:h-full overflow-auto lg:w-1/2 flex flex-col items-start justify-around px-1 md:px-4 lg:px-8 xl:px-16 bg-gradient-to-br">
       <div className=" w-full flex flex-col">
-        <h3 className=" text-white text-[28px] lg:text-xl mb-5 lg:mb-9 font-semibold">
+        <h3 className=" text-white text-[28px] lg:text-xl mb-3 font-semibold">
           Loan details
         </h3>
 
@@ -33,7 +33,7 @@ const CreateLeft = () => {
 
       {/* PURPOSE */}
       <div className=" w-full flex flex-col">
-        <h3 className=" text-white text-[28px] lg:text-xl mb-5 lg:mb-9 font-semibold">
+        <h3 className=" text-white text-[28px] lg:text-xl mb-3 font-semibold">
           About
         </h3>
 
@@ -48,26 +48,28 @@ const CreateLeft = () => {
         </div>
       </div>
 
-      {/* INTEREST RATE */}
+      {/* PRINCIPLE AMOUNT */}
       <div className=" w-full flex flex-col">
         <h3 className=" text-white text-[28px] lg:text-xl mb-5 lg:mb-9">
-          Loan details
+          Terms Configurations
         </h3>
 
-        {/* TITLE */}
-        <div className=" w-full h-full flex flex-col items-start ">
-          <label className="labelStyles" htmlFor="title">
-            Title
+        {/* Inputs */}
+        <div className=" w-full h-full flex flex-col items-start">
+          <label className="labelStyles" htmlFor="inputs">
+            Principle Amount & Interest %
           </label>
-          <input
-            type="text"
-            placeholder="Enter a title for your loan"
-            className="inputField"
-          />
+          <section className=" w-full h-[30vh] border-2 border-white flex flex-col lg:flex-row gap-2 xl:gap-4 items-start ">
+            <div className=" w-full h-[40%] border-2 border-neutral-400 rounded-md flex flex-col items-start px-1 md:px-2 lg:px-3 py-2 ">
+              <label htmlFor="amount">Amount</label>
+              <input type="slider" />
+            </div>
+            <div className=" w-full h-[40%] border-2 border-neutral-400 rounded-md "></div>
+          </section>
         </div>
       </div>
 
-      {/* PRINCIPLE AMOUNT */}
+      {/* INTEREST RATE */}
 
       {/* INSTALMENT AMOUNT */}
 
