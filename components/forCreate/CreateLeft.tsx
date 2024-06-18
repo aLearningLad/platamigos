d053b2d0bb57aacf4ec96acee85db59359179103
aLearningLad <mocmanca@gmail.com>
2024-06-18 21:05:28 +0200

    interest rate additions

--- components/forCreate/CreateLeft.tsx
+++ components/forCreate/CreateLeft.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 const CreateLeft = () => {
   const [title, setTitle] = useState<string>("");
@@ -10,6 +10,16 @@ const CreateLeft = () => {
   const [principle, setPrinciple] = useState<number | null>(null);
   const [instalment, setInstalment] = useState<number | null>(null);
   const [totalDue, setTotalDue] = useState<number | null>(null);
+  const [interestValue, setInterestValue] = useState({
+    unitValue: 0,
+    tensValue: 0,
+    firstDecimal: 0,
+    secondDecimal: 0,
+  });
+
+  const handleInterestChanges = (e: ChangeEvent<HTMLInputElement>) => {
+    setInterestValue((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
   return (
     <section className="w-full h-[70vh] md:h-[65vh] lg:h-full overflow-auto lg:w-1/2 flex flex-col items-start justify-around px-1 md:px-4 lg:px-8 xl:px-16 bg-gradient-to-br">
@@ -60,10 +70,12 @@ const CreateLeft = () => {
             Principle Amount & Interest %
           </label>
           <section className=" w-full h-[30vh] border-2 border-white flex flex-col lg:flex-row gap-2 xl:gap-4 items-start ">
-            <div className=" w-full h-[40%] border-2 lg:w-8/12 border-neutral-400 rounded-md flex flex-col items-start px-1 md:px-2 lg:px-3 py-2 ">
-              <label htmlFor="amount">Amount</label>
+            <div className=" w-full group cursor-pointer h-[50%] border-2 lg:w-8/12 border-neutral-400 rounded-md flex flex-col items-start px-1 md:px-2 lg:px-3 py-2 ">
+              <label className="labelStyles" htmlFor="amount">
+                Amount
+              </label>
               <span className=" flex flex-row w-full items-center gap-[1px] lg:gap-[2px]  ">
-                <p className="text-neutral-200 text-[12px] lg:text-[10px] w-fit">
+                <p className="text-neutral-200 text-[12px] lg:text-[10px] w-fit group-hover:text-white group-hover:text-[15px] transition-all duration-300 ease-in-out ">
                   R300
                 </p>
                 <input
@@ -73,14 +85,81 @@ const CreateLeft = () => {
                   min={300}
                   max={5000}
                   step={100}
+                  value={Number(principle)}
                   className="rangeInput"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setPrinciple(Number(e.target.value))
+                  }
                 />
-                <p className="text-neutral-200 text-[12px] lg:text-[10px] w-fit">
+                <p className="text-neutral-200 text-[12px] lg:text-[10px] w-fit group-hover:text-white group-hover:text-[15px] transition-all duration-300 ease-in-out">
                   R5000
                 </p>
               </span>
+              <span className=" w-full py-1 flex justify-center items-center">
+                <div className=" text-[15px] flex items-center justify-center group-hover:px-3 lg:group-hover:px-5 py-1 group-hover:text-[12px] text-white group-hover:bg-white group-hover:text-black rounded-md transition-all duration-300 ease-in">
+                  <p className=" group-hover:hidden mr-1">~</p> R{principle}
+                </div>
+              </span>
             </div>
-            <div className=" w-full h-[40%] lg:w-4/12 border-2 border-neutral-400 rounded-md "></div>
+            <div className="w-full group cursor-pointer h-[50%] lg:w-4/12 border-2 border-neutral-400 rounded-md flex flex-col items-start px-1 md:px-2 lg:px-3 py-2">
+              <label className="labelStyles" htmlFor="interest">
+                Interest rate
+              </label>
+              <span className=" w-full h-full text-white flex items-end">
+                <input
+                  type="number"
+                  value={interestValue.unitValue}
+                  name="unitValue"
+                  className="numberInput mr-1"
+                  min={0}
+                  max={9}
+                />
+                <input
+                  type="number"
+                  value={interestValue.tensValue}
+                  name="tensValue"
+                  className="numberInput"
+                  min={0}
+                  max={9}
+                  onChange={handleInterestChanges}
+                />
+                <p className=" text-2xl font-semibold mx-1">.</p>
+                <input
+                  type="number"
+                  className="numberInput mr-1"
+                  value={interestValue.firstDecimal}
+                  name="firstDecimal"
+                  min={0}
+                  max={9}
+                  onChange={handleInterestChanges}
+                />
+                <input
+                  type="number"
+                  value={interestValue.secondDecimal}
+                  name="secondDecimal"
+                  className="numberInput"
+                  min={0}
+                  max={9}
+                  onChange={handleInterestChanges}
+                />
+              </span>
+              <span className="w-full h-full text-white flex justify-center items-end py-1">
+                <p className=" text-white text-[13px] lg:text-[12px]">
+                  {interestValue.unitValue}
+                </p>
+                <p className=" text-white text-[13px] lg:text-[12px]">
+                  {interestValue.tensValue}
+                </p>
+                <p className=" text-2xl font-semibold">.</p>
+                <p className=" text-white text-[13px] lg:text-[12px]">
+                  {interestValue.firstDecimal}
+                </p>
+                <p className=" text-white text-[13px] lg:text-[12px]">
+                  {interestValue.secondDecimal}
+                </p>
+                <p className="text-lg text-neutral-200">%</p>
+              </span>
+            </div>
           </section>
         </div>
       </div>
