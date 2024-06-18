41b5af1eb4db93d77dfc034546d4da572ade48dd
aLearningLad <mocmanca@gmail.com>
2024-06-18 17:13:14 +0200

    commit page additions

--- components/forCreate/CreateLeft.tsx
+++ components/forCreate/CreateLeft.tsx
@@ -12,16 +12,60 @@ const CreateLeft = () => {
   const [totalDue, setTotalDue] = useState<number | null>(null);
 
   return (
-    <section className="w-full h-[70vh] md:h-[65vh] lg:h-full lg:w-1/2 flex flex-col items-center justify-around p-1 md:p-2 lg:p-4 xl:p-7 border-2 border-white bg-gradient-to-br from-cyan-800/50 via-black to-black">
-      {/* TITLE */}
-      <div className=" w-full h-full flex flex-col items-start ">
-        <label htmlFor="title">Loan Title</label>
-        <input type="text" className="inputField" />
+    <section className="w-full h-[70vh] md:h-[65vh] lg:h-full lg:w-1/2 flex flex-col items-start justify-around p-1 md:p-2 lg:p-4 xl:p-7 border-2 border-white bg-gradient-to-br from-cyan-800/50 via-black to-black">
+      <div className=" w-full flex flex-col">
+        <h3 className=" text-white text-[28px] lg:text-xl mb-5 lg:mb-9 font-semibold">
+          Loan details
+        </h3>
+
+        {/* TITLE */}
+        <div className=" w-full h-full flex flex-col items-start ">
+          <label className="labelStyles" htmlFor="title">
+            Title
+          </label>
+          <input
+            type="text"
+            placeholder="Enter a title for your loan"
+            className="inputField"
+          />
+        </div>
       </div>
 
       {/* PURPOSE */}
+      <div className=" w-full flex flex-col">
+        <h3 className=" text-white text-[28px] lg:text-xl mb-5 lg:mb-9 font-semibold">
+          About
+        </h3>
+
+        <div className=" w-full h-full flex flex-col items-start ">
+          <label className="labelStyles" htmlFor="title">
+            Additional Info on Offer
+          </label>
+          <textarea
+            placeholder="Provide information on your loan offer"
+            className="textarea"
+          />
+        </div>
+      </div>
 
       {/* INTEREST RATE */}
+      <div className=" w-full flex flex-col">
+        <h3 className=" text-white text-[28px] lg:text-xl mb-5 lg:mb-9">
+          Loan details
+        </h3>
+
+        {/* TITLE */}
+        <div className=" w-full h-full flex flex-col items-start ">
+          <label className="labelStyles" htmlFor="title">
+            Title
+          </label>
+          <input
+            type="text"
+            placeholder="Enter a title for your loan"
+            className="inputField"
+          />
+        </div>
+      </div>
 
       {/* PRINCIPLE AMOUNT */}
 
--- components/forCreate/CreateLoanComp.tsx
+++ components/forCreate/CreateLoanComp.tsx
@@ -7,11 +7,11 @@ const CreateLoanComp = () => {
   return (
     <div className="flex-col  flex justify-center items-center w-full h-full">
       <header className=" w-full flex h-fit py-3 lg:py-4 justify-center items-center">
-        <h1 className=" text-4xl md:text-3xl lg:text-2xl text-white">
-          Loan Offer Details
+        <h1 className=" text-4xl md:text-3xl lg:text-4xl text-white">
+          Create an Offer
         </h1>
       </header>
-      <div className=" flex flex-col lg:flex-row">
+      <div className=" flex flex-col lg:flex-row w-full h-full">
         <CreateLeft />
         <CreateRight />
       </div>
