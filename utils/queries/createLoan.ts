9a572a5f2dc06f2a9ec77f3dc6e87a142dbef0df
aLearningLad <mocmanca@gmail.com>
2024-06-19 23:46:36 +0200

    create loan util function logic draft

--- utils/queries/createLoan.ts
+++ utils/queries/createLoan.ts
@@ -5,9 +5,35 @@ import { createClient } from "../supabase/client";
 
 export const createLoan = async (params: { params: TcreateLoanParams }) => {
   const supabase = createClient();
+  const lenderId = (await supabase.auth.getUser()).data.user?.id;
+  const lenderName = await supabase
+    .from("allUsers")
+    .select("username")
+    .eq("googleid", lenderId);
+  const expiryDateToDb = new Date(params.params.expiry_date);
+
   try {
-    const { error: createLoanError } = await supabase
-      .from("homeFeed")
-      .insert({});
-  } catch (error) {}
+    const { error: createLoanError } = await supabase.from("homefeed").insert({
+      lenderid: lenderId,
+      lendername: lenderName,
+      title: params.params.title,
+      purpose: params.params.purpose,
+      // date_posted: new Date().getDate(), CHECK IF AUTO INJECTED, CORRECT IF NOT
+      expiry_date: expiryDateToDb,
+      interest_rate: params.params.interest_rate,
+      principal_offer: params.params.principal_offer, //AWFUL SPELLING.
+      instalment: params.params.instalment,
+      total: params.params.total_due,
+    });
+
+    if (!createLoanError) {
+      alert("Loan listing created!"); //REPLACE WITH A TOAST
+    }
+
+    if (createLoanError) {
+      throw new Error(createLoanError.message);
+    }
+  } catch (error) {
+    console.log("Error while creating loan listing: ", error);
+  }
 };
