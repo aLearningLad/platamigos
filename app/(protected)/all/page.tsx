fd8f9a06d650e513619bd4d9a70688f0123a4b75
aLearningLad <mocmanca@gmail.com>
2024-06-24 13:52:13 +0200

    loan card added

--- app/(protected)/all/page.tsx
+++ app/(protected)/all/page.tsx
@@ -1,7 +1,23 @@
+import { LoanCard } from "@/components/forLoansPage";
 import React from "react";
 
 const AllLoansPage = () => {
-  return <div>AllLoansPage</div>;
+  const falseData = [1, 2, 3, 4, 5, 6, 7];
+
+  return (
+    <div className=" w-full h-full flex flex-col border-4 border-red-600">
+      <header className=" w-full float-start justify-center items-center text-center py-2 md:py-3 lg:py-5 border-2 border-white">
+        <h1 className=" text-5xl font-semibold text-white">
+          All Your Published Loans
+        </h1>
+      </header>
+      <section className=" w-full h-full p-1 lg:p-3 xl:p-5 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 overflow-auto gap-2 md:gap-3 lg:gap-5">
+        {falseData.map((card) => (
+          <LoanCard />
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default AllLoansPage;
---
+++ components/forLoansPage/LoanCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoanCard = () => {
+  return <div className=" w-full h-[60vh] bg-white ">LoanCard</div>;
+};
+
+export default LoanCard;
