ff3b067417a1b136cf15ffb4bfc347495129ca64
aLearningLad <mocmanca@gmail.com>
2024-06-24 23:44:14 +0200

    packages tinkering

--- components/forDash/DashMiddleTop.tsx
+++ components/forDash/DashMiddleTop.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { FcMoneyTransfer } from "react-icons/fc";
 import DashBalance from "./DashBalance";
+import DoughnutChart from "./DoughnutChart";
 
 const DashMiddleTop = () => {
   return (
@@ -11,7 +12,9 @@ const DashMiddleTop = () => {
       {/* TOTAL BORROWED & DISBURSED PIE */}
       <section className=" w-full bg-gradient-to-br border-4 border-transparent hover:scale-95 transition-all duration-300 ease-in-out hover:border-neutral-400 from-blue-950 via-purple-950/40 to-blue-900/20 items-start text-start p-1 md:p-2 lg:p-5 h-full rounded-lg lg:rounded-xl xl:rounded-[22px] flex flex-col text-white">
         <h1 className="dashlabels">Your Splits</h1>
-        <div className=" h-full w-full bg-white"></div>
+        <div className=" h-full w-full bg-transparent">
+          <DoughnutChart />
+        </div>
       </section>
 
       {/* TINY GRAPHS & FIGURES */}
--- components/forDash/index.ts
+++ components/forDash/index.ts
@@ -3,3 +3,4 @@ export { default as DashEnd } from "./DashEnd";
 export { default as DashMiddleTop } from "./DashMiddleTop";
 export { default as DashMiddleBottom } from "./DashMiddleBottom";
 export { default as DashBalance } from "./DashBalance";
+export { default as DoughnutChart } from "./DoughnutChart";
