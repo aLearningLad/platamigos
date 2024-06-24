661098a064563353f5826be119e46b2626811707
aLearningLad <mocmanca@gmail.com>
2024-06-24 16:16:51 +0200

    accept loan util updates

--- utils/myFxns/acceptLoan.ts
+++ utils/myFxns/acceptLoan.ts
@@ -1,5 +1,27 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+import { createClient } from "../supabase/server";
+
 export const acceptLoan = async (loanId: string) => {
-  console.log("accepted this loan!", loanId);
+  const supabase = createClient();
+  try {
+    const { error: acceptLoanError } = await supabase
+      .from("homefeed")
+      .update({
+        disbursed: true,
+      })
+      .eq("loan_id", loanId);
+
+    if (!acceptLoanError) {
+      revalidatePath("/all");
+      console.log(`${loanId} accepted!`);
+    }
+
+    if (acceptLoanError) {
+      throw new Error(acceptLoanError.details);
+    }
+  } catch (error) {
+    console.log("Error while accepting loan: ", error);
+  }
 };
