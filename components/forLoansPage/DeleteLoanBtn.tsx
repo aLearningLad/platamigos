908c827220458bc41ce2c50e38eaf799d5804221
aLearningLad <mocmanca@gmail.com>
2024-06-25 06:29:00 +0200

    loan card progress

--- components/forLoansPage/DeleteLoanBtn.tsx
+++ components/forLoansPage/DeleteLoanBtn.tsx
@@ -4,7 +4,14 @@ import { Ideleteloantbn } from "@/interfaces";
 import { deleteLoan } from "@/utils/myFxns/deleteLoan";
 
 const DeleteLoanBtn: React.FC<Ideleteloantbn> = ({ loanid }) => {
-  return <button onClick={() => deleteLoan(loanid)}>Delete {loanid}</button>;
+  return (
+    <button
+      className="text-2xl w-full py-2 mt-2 md:mt-4 lg:mt-6 rounded-md lg:rounded-lg bg-cyan-600 text-white"
+      onClick={() => deleteLoan(loanid)}
+    >
+      Remove Listing
+    </button>
+  );
 };
 
 export default DeleteLoanBtn;
--- components/forLoansPage/LoanCard.tsx
+++ components/forLoansPage/LoanCard.tsx
@@ -27,29 +27,29 @@ const LoanCard: React.FC<Tloansfromdb> = async ({
   const watcher_id: string = result.user.id;
 
   return (
-    <div className="w-full h-[60vh] lg:h-[75vh] bg-white rounded-md py-2 md:py-3 lg:py-5 px-2 lg:px-4 md:rounded-lg lg:rounded-xl xl:rounded-2xl flex items-center text-black flex-col text-center lg:text-start">
+    <div className="w-full bg-white rounded-md py-2 md:py-3 lg:py-5 px-2 lg:px-4 md:rounded-lg lg:rounded-xl xl:rounded-2xl flex items-center text-black flex-col text-center lg:text-start">
       <header className="w-full border-b-2 border-neutral-200 flex flex-col text-center lg:text-start">
         <h1 className="text-ellipsis font-semibold text-3xl lg:text-2xl text-black">
           {title}
         </h1>
-        <p className="text-xl lg:text-[14px] text-neutral-800 ">{purpose}</p>
+        <p className="text-xl lg:text-[14px] text-neutral-800">{purpose}</p>
       </header>
 
-      <section className=" mt-2 md:mt-4 lg:mt-5 flex flex-col items-start w-full">
-        <h3 className=" text-[12px] font-semibold ">Amount Offered</h3>
-        <h1 className=" text-3xl font-extrabold xl:text-5xl ">
+      <section className="mt-2 md:mt-4 lg:mt-5 flex flex-col items-start w-full">
+        <h3 className="text-[12px] font-semibold">Amount Offered</h3>
+        <h1 className="text-3xl font-extrabold xl:text-5xl">
           R{principal_offer}
         </h1>
-        <p className=" text-[12px] font-semibold">@ {interest_rate}%</p>
-        <p>over 4 {months} months</p>
+        <p className="text-[12px] font-semibold">@ {interest_rate}%</p>
+        <p>over {months} months</p>
       </section>
 
-      <section className=" mt-2 lg:mt-5 flex flex-col w-full text-center lg:text-start">
-        <h3 className=" text-lg font-semibold">Validity</h3>
+      <section className="mt-2 lg:mt-5 flex flex-col w-full text-center lg:text-start">
+        <h3 className="text-lg font-semibold">Validity</h3>
         <p>This offer is due to expire on {expiry_date as string}</p>
       </section>
 
-      <section className=" mt-2 lg:mt-4 flex flex-col text-center lg:text-start w-full">
+      <section className="mt-2 lg:mt-4 flex flex-col text-center lg:text-start w-full">
         <div>
           <h2>Potential Returns</h2>
           <p>R{total_due}</p>
@@ -60,18 +60,18 @@ const LoanCard: React.FC<Tloansfromdb> = async ({
         </div>
       </section>
 
-      <section className=" w-full mt-2 flex flex-col gap-2 lg:gap-4">
+      <section className="w-full mt-2 flex flex-col gap-2 lg:gap-4">
         <div>Loan rating: A+</div>
         <div>Applicants: 223</div>
       </section>
 
       <DeleteLoanBtn loanid={loan_id} />
 
-      {/* 
-      <AcceptLoanBtn loanId={loanid} />
+      {/* Uncomment these lines if needed
+      <AcceptLoanBtn loanId={loan_id} />
       <TrackedSymbol />
-      <TrackLoanBtn loan_id={loanid} watcher_id={watcher_id} />
-      <Link href={`/all/${loanid}`}>Tweak Offer</Link> */}
+      <TrackLoanBtn loan_id={loan_id} watcher_id={watcher_id} />
+      <Link href={`/all/${loan_id}`}>Tweak Offer</Link> */}
     </div>
   );
 };
