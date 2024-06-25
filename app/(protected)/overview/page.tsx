5bc8ffcfcd79c261956322d74859f66ea1c006ab
aLearningLad <mocmanca@gmail.com>
2024-06-25 16:09:26 +0200

    progress

--- app/(protected)/overview/page.tsx
+++ app/(protected)/overview/page.tsx
@@ -3,8 +3,9 @@ import React from "react";
 
 const Overview = () => {
   return (
-    <div className=" w-full h-full flex flex-col lg:flex-row">
+    <div className=" w-full h-full flex flex-col lg:flex-row text-white relative">
       <OverviewDisbursed />
+      <div className="w-[2px] bg-neutral-600 rounded-full hidden lg:flex  sticky " />
       <OverviewDebts />
     </div>
   );
--- components/forOverview/OverviewDebts.tsx
+++ components/forOverview/OverviewDebts.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const OverviewDebts = () => {
   return (
-    <div className=" w-full lg:w-1/2 flex flex-col items-center lg:items-start px-2 md:px-3 lg:px-5 xl:px-7 py-3 lg:py-5">
-      OverviewDebts
+    <div className=" w-full relative lg:w-1/2 flex flex-col items-center lg:items-start px-2 md:px-3 lg:px-5 xl:px-7 py-3 lg:py-5">
+      <header className=" w-full sticky top-0 bg-black">
+        <h1 className=" text-3xl font-semibold">Debts</h1>
+      </header>
     </div>
   );
 };
