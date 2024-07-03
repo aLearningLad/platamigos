d4d6fb89d5707174a9ca59813585346a2c85a698
aLearningLad <mocmanca@gmail.com>
2024-07-03 17:54:16 +0200

    application handling logic

--- components/foruserhome/ToDisburseBody.tsx
+++ components/foruserhome/ToDisburseBody.tsx
@@ -3,10 +3,14 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 import { Tpending, Ttodisburse } from "@/types";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 const ToDisburseBody = () => {
   const [toDisburse, setToDisburse] = useState<any[]>();
   const [isLoading, setIsLoading] = useState(true);
+  const [oldBalance, setOldBalance] = useState<number>();
+  const router = useRouter();
 
   useEffect(() => {
     const getResult = async () => {
@@ -27,6 +31,11 @@ const ToDisburseBody = () => {
           console.log("This is data to disburse: ", toDisburseData);
           setToDisburse(toDisburseData);
         }
+        const { data: DbBalance, error: DbBalanceError } = await supabase
+          .from("allusers")
+          .select("current_balance")
+          .eq("googleid", googleid);
+        setOldBalance(DbBalance![0].current_balance);
       } catch (error) {
         console.log("Error fetching to disburse: ", error);
       }
@@ -45,39 +54,141 @@ const ToDisburseBody = () => {
   ) => {
     const supabase = createClient();
     const googleid = (await supabase.auth.getUser()).data.user?.id;
-    try {
-      // UPDATE HOMEFEED 'disbursed' COLUMN ==> BRAVO!
-      const { error: homefeedUpdateError } = await supabase
-        .from("homefeed")
-        .update({
-          disbursed: true,
-        });
 
-      // DELETE ENTRY FROM 'pending' ===> BRAVO
-      const { error: deleteFromPendingError } = await supabase
-        .from("pending")
-        .delete()
-        .eq("loanid", loanid);
+    if (oldBalance && oldBalance > principal_amount) {
+      try {
+        // UPDATE HOMEFEED 'disbursed' COLUMN ==> BRAVO!
+        const { error: homefeedUpdateError } = await supabase
+          .from("homefeed")
+          .update({
+            disbursed: true,
+          });
 
-      // DELETE ENTRY FROM 'to_disburse'
-      const { error: deleteFromDisburseError } = await supabase
-        .from("to_disburse")
-        .delete()
-        .eq("loanid", loanid);
+        // DELETE ENTRY FROM 'pending' ===> BRAVO
+        const { error: deleteFromPendingError } = await supabase
+          .from("pending")
+          .delete()
+          .eq("loanid", loanid);
 
-      // CREATE ENTRY IN 'granted_loans'
-      const { data: granted_data, error: grantedError } = await supabase
-        .from("granted_loans")
-        .insert({
-          applicant_id,
-          lender_id: googleid,
-          loan_id: loanid,
-          principal_amount,
-          applicant_name,
-          total_due,
-          interest_rate,
-        });
-    } catch (error) {}
+        // DELETE ENTRY FROM 'to_disburse'
+        const { error: deleteFromDisburseError } = await supabase
+          .from("to_disburse")
+          .delete()
+          .eq("loanid", loanid);
+
+        // CREATE ENTRY IN 'granted_loans'
+        const { data: granted_data, error: grantedError } = await supabase
+          .from("granted_loans")
+          .insert({
+            applicant_id,
+            lender_id: googleid,
+            loan_id: loanid,
+            principal_amount,
+            applicant_name,
+            total_due,
+            interest_rate,
+          });
+
+        // DEDUCT AMOUNT FROM GRANTOR'S BALANCE IN allusers TABLE
+        const newBalance = oldBalance! - principal_amount;
+        const { error: updateGrantorBalanceError } = await supabase
+          .from("allusers")
+          .update({
+            current_balance: newBalance,
+          })
+          .eq("googleid", googleid);
+
+        if (updateGrantorBalanceError) {
+          throw new Error(updateGrantorBalanceError.details);
+        }
+
+        // ADD AMOUNT TO APPLICANT'S BALANCE IN homefeed TABLE
+        const { data: applicantFunds, error: applicantFundsError } =
+          await supabase
+            .from("allusers")
+            .select("current_balance")
+            .eq("googleid", applicant_id);
+        const applicant_new_balance =
+          applicantFunds![0].current_balance + principal_amount;
+        const { error: updateGranteeBalanceError } = await supabase
+          .from("allusers")
+          .update({
+            current_balance: applicant_new_balance,
+          })
+          .eq("googleid", applicant_id);
+
+        // ADD PRINCIPAL TO LENDER'S total_disbursed COLUMN IN allusers
+        const { data: totalDisbursedAmount, error: totalDisbursedAmountError } =
+          await supabase
+            .from("allusers")
+            .select("total_disbursed")
+            .eq("googleid", googleid);
+        const new_total_disbursed =
+          totalDisbursedAmount![0].total_disbursed + principal_amount;
+        const { error: updateTotalDisbursedError } = await supabase
+          .from("allusers")
+          .update({
+            total_disbursed: new_total_disbursed,
+          })
+          .eq("googleid", googleid);
+
+        if (updateTotalDisbursedError) {
+          throw new Error(updateTotalDisbursedError.details);
+        }
+
+        // ADD total_due TO APPLICANT'S total_borrowed COLUMN IN allusers
+        const { data: total_borrowed, error: total_borrowedError } =
+          await supabase
+            .from("allusers")
+            .select("total_borrowed")
+            .eq("googleid", applicant_id);
+        const new_total_borrowed =
+          total_borrowed![0].total_borrowed + total_due;
+
+        const { error: updateTotalBorrowedError } = await supabase
+          .from("allusers")
+          .update({
+            total_borrowed: new_total_borrowed,
+          });
+
+        if (updateTotalBorrowedError) {
+          throw new Error(updateTotalBorrowedError.details);
+        }
+
+        // ADD total_due TO APPLICANT'S total_outstanding COLUMN IN allusers
+
+        if (updateGranteeBalanceError) {
+          throw new Error(updateGranteeBalanceError.details);
+        }
+
+        if (homefeedUpdateError) {
+          throw new Error(homefeedUpdateError.details);
+        }
+        if (deleteFromPendingError) {
+          throw new Error(deleteFromPendingError.details);
+        }
+        if (deleteFromDisburseError) {
+          throw new Error(deleteFromDisburseError.details);
+        }
+        if (grantedError) {
+          throw new Error(grantedError.details);
+        }
+
+        if (
+          !homefeedUpdateError &&
+          !deleteFromPendingError &&
+          !deleteFromDisburseError &&
+          !grantedError
+        ) {
+          router.refresh();
+          toast.success("Granted!");
+        }
+      } catch (error) {
+        console.log("Error while granting loan: ", error);
+      }
+    } else {
+      toast.error("Insufficient funds!");
+    }
   };
 
   if (isLoading) {
@@ -139,10 +250,20 @@ const ToDisburseBody = () => {
               </section>
             </div>
             <button
-              // onClick={handleGrant}
+              onClick={() =>
+                handleGrant(
+                  item.loanid,
+                  item.applicant_id,
+                  item.principal,
+                  item.applicant_name,
+                  item.total_due,
+                  item.interest_rate
+                )
+              }
               className=" w-10/12 py-2 mt-1 hover:bg-white hover:border-white hover:font-extrabold hover:scale-95 transition-all duration-300 ease-in-out text-lg bg-gradient-to-tr rounded-lg from-purple-800/50 via-cyan-600/70 to-cyan-700/90 text-white font-semibold"
             >
               Grant
+              {/* {oldBalance} */}
             </button>
           </div>
         ))}
